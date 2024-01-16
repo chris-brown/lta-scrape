3e212277992627f2b7fcd679acd440749ce2045a
Chris Brown <cj_browna@hotmail.com>
2024-01-16 21:45:07 +0000

    fixing page numbers post NY

--- cypress/e2e/lta-website.cy.js
+++ cypress/e2e/lta-website.cy.js
@@ -1,10 +1,8 @@
 describe("Scrape LTA website", () => {
-
-  const writeToFile = title => (csvContent) => {
-      cy.writeFile(
-        `files/${title}.csv`,
-        ["player,id,year,county,form,tournaments", ...csvContent].join("\n")
-      );
+  const writeToFile =
+    (title, headers = "player,id,year,county,form,tournaments") =>
+    (csvContent) => {
+      cy.writeFile(`files/${title}.csv`, [headers, ...csvContent].join("\n"));
     };
 
   const scrape = (csvContent, page, maxPage) => {
@@ -26,7 +24,7 @@ describe("Scrape LTA website", () => {
             .then(() => {
               page++;
               cy.get('a[title="Next"]', { force: true }).click();
-              if(page < maxPage) return checkButtonAndLoop();
+              if (page < maxPage) return checkButtonAndLoop();
             });
         } else {
           return;
@@ -35,60 +33,56 @@ describe("Scrape LTA website", () => {
     };
 
     return checkButtonAndLoop().then(() => csvContent);
-  }
+  };
 
   it("u9", () => {
-    cy.visit(
-      "https://competitions.lta.org.uk/ranking/ranking.aspx?rid=303"
-    );
+    cy.visit("https://competitions.lta.org.uk/ranking/ranking.aspx?rid=303");
     cy.contains("Accept").click();
-    cy.contains('a', '9U Girls').click();
-    cy.get('select#_pagesize').select('100');
+    cy.contains("a", "9U Girls").click();
+    cy.get("select#_pagesize").select("100");
 
     const csvContent = [];
     let page = 1;
-    const maxPage = 7;
+    const maxPage = 4;
 
-    scrape(csvContent, page, maxPage).then(writeToFile('u9'));
+    scrape(csvContent, page, maxPage).then(writeToFile("u9"));
   });
 
   it("u10", () => {
-    cy.visit(
-      "https://competitions.lta.org.uk/ranking/ranking.aspx?rid=303"
-    );
+    cy.visit("https://competitions.lta.org.uk/ranking/ranking.aspx?rid=303");
     cy.contains("Accept").click();
-    cy.contains('a', '10U Girls').click();
-    cy.get('select#_pagesize').select('100');
+    cy.contains("a", "10U Girls").click();
+    cy.get("select#_pagesize").select("100");
 
     const csvContent = [];
     let page = 1;
-    const maxPage = 7;
+    const maxPage = 4;
 
-    scrape(csvContent, page, maxPage).then(writeToFile('u10'));
+    scrape(csvContent, page, maxPage).then(writeToFile("u10"));
   });
 
   it("combined", () => {
-    cy.visit(
-      "https://competitions.lta.org.uk/ranking/ranking.aspx?rid=301"
-    );
+    cy.visit("https://competitions.lta.org.uk/ranking/ranking.aspx?rid=301");
     cy.contains("Accept").click();
-    cy.contains('a', '11U Girls').click();
-    cy.get('select#_pagesize').select('100', { force: true });
+    cy.contains("a", "11U Girls").click();
+    cy.get("select#_pagesize").select("100", { force: true });
 
     const csvContent = [];
     let page = 1;
-    const maxPage = 6;
 
-    const content = scrape(csvContent, page, maxPage);
+    const content = scrape(csvContent, page, 4);
 
-    console.log('content', content)
+    console.log("content", content);
 
-    cy.visit(
-      "https://competitions.lta.org.uk/ranking/ranking.aspx?rid=301"
+    cy.visit("https://competitions.lta.org.uk/ranking/ranking.aspx?rid=301");
+
+    cy.contains("a", "11U Boys").click();
+    cy.get("select#_pagesize").select("100", { force: true });
+    scrape(csvContent, page, 5).then(
+      writeToFile(
+        "combined",
+        "rank,rank_up,empty,player,id,year,county,singles,doubles,tournaments,tournamentsused,form"
+      )
     );
-
-    cy.contains('a', '11U Boys').click();
-    cy.get('select#_pagesize').select('100', { force: true });
-    scrape(csvContent, page, maxPage).then(writeToFile('combined'));
   });
 });
